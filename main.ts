2c857513376e423f01deff5208db17cb146f30b8
klhrd <93413479+klhrd@users.noreply.github.com>
2022-02-05 11:22:01 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,35 +1,65 @@
+let 車車車車車1: number[] = []
+let 車子list: game.LedSprite[] = []
+let 車車車車車2: number[] = []
+let 車車車車車3: number[] = []
+let frog: game.LedSprite = null
+let 分身號 = 0
+let 撞飛號 = 0
 function cars () {
-    list.push(game.createSprite(4, randint(1, 3)))
+    if (車車車車車1.pop() == 1) {
+        車子list.push(game.createSprite(4, 1))
+    }
+    if (車車車車車2.pop() == 1) {
+        車子list.push(game.createSprite(4, 2))
+    }
+    if (車車車車車3.pop() == 1) {
+        車子list.push(game.createSprite(4, 3))
+    }
 }
 input.onButtonPressed(Button.A, function () {
     frog.change(LedSpriteProperty.X, -1)
-})
-input.onLogoEvent(TouchButtonEvent.Pressed, function () {
-    list.push(game.createSprite(4, randint(1, 3)))
+    分身號 = 0
+    for (let index = 0; index < 車子list.length; index++) {
+        if (frog.isTouching(車子list[撞飛號])) {
+            game.removeLife(1)
+        }
+        撞飛號 += 1
+    }
 })
 input.onButtonPressed(Button.AB, function () {
     frog.change(LedSpriteProperty.Y, -1)
+    分身號 = 0
+    for (let index = 0; index < 車子list.length; index++) {
+        if (frog.isTouching(車子list[撞飛號])) {
+            game.removeLife(1)
+        }
+        撞飛號 += 1
+    }
 })
 input.onButtonPressed(Button.B, function () {
     frog.change(LedSpriteProperty.X, 1)
+    分身號 = 0
+    for (let index = 0; index < 車子list.length; index++) {
+        if (frog.isTouching(車子list[撞飛號])) {
+            game.removeLife(1)
+        }
+        撞飛號 += 1
+    }
 })
-let 分身號 = 0
-let 車車車車車1: string[] = []
-let list: game.LedSprite[] = []
-let frog: game.LedSprite = null
-let 車子list: number[] = []
-frog = game.createSprite(2, 4)
 basic.forever(function () {
     車車車車車1 = []
-    frog.delete()
+    車車車車車2 = []
+    車車車車車3 = []
+    車子list = []
+    game.setLife(3)
     frog = game.createSprite(2, 4)
     while (!(frog.get(LedSpriteProperty.Y) == 0)) {
         分身號 = 0
-        for (let index = 0; index < list.length; index++) {
-            if (list[分身號].get(LedSpriteProperty.X) == 0) {
-                list[分身號].delete()
+        for (let index = 0; index < 車子list.length; index++) {
+            if (車子list[分身號].get(LedSpriteProperty.X) == 0) {
+                車子list[分身號].delete()
             }
-            list[分身號].change(LedSpriteProperty.X, -1)
+            車子list[分身號].change(LedSpriteProperty.X, -1)
             分身號 += 1
         }
         cars()
@@ -38,12 +68,22 @@ basic.forever(function () {
     game.addScore(1)
 })
 basic.forever(function () {
-    let 車車車車車3: string[] = []
-    let 車車車車車2: string[] = []
-    車車車車車1.push("c" + convertToText(randint(1, 4)))
-    車車車車車1.push("s" + convertToText(randint(1, 4)))
-    車車車車車2.push("c" + convertToText(randint(1, 4)))
-    車車車車車2.push("s" + convertToText(randint(1, 4)))
-    車車車車車3.push("c" + convertToText(randint(1, 4)))
-    車車車車車3.push("s" + convertToText(randint(1, 4)))
+    for (let index = 0; index < randint(1, 3); index++) {
+        車車車車車1.unshift(1)
+    }
+    for (let index = 0; index < randint(2, 4); index++) {
+        車車車車車1.unshift(0)
+    }
+    for (let index = 0; index < randint(1, 3); index++) {
+        車車車車車2.unshift(1)
+    }
+    for (let index = 0; index < randint(2, 4); index++) {
+        車車車車車2.unshift(0)
+    }
+    for (let index = 0; index < randint(1, 3); index++) {
+        車車車車車3.unshift(1)
+    }
+    for (let index = 0; index < randint(2, 4); index++) {
+        車車車車車3.unshift(0)
+    }
 })
